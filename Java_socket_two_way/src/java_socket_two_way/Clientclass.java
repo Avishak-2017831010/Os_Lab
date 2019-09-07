94aed12595e89960dfcef2128a48932abe5928d7
Avishak Chakroborty <aurgho1998@gmail.com>
2019-09-08 02:12:53 +0600

    Add files via upload

---
+++ Java_socket_two_way/src/java_socket_two_way/Clientclass.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author SHUBHAM
+ */
+public class Clientclass {
+
+    
+    public static void main(String[] args) throws SocketException, IOException {
+        
+      BufferedReader clientRead =new BufferedReader(new InputStreamReader(System.in));
+      Scanner sc=new Scanner(System.in);
+      InetAddress IP = InetAddress.getByName("10.100.174.83");
+      DatagramSocket clientSocket = new DatagramSocket();
+      while(true)    
+      {
+      byte[] sendbuffer = new byte[1024];
+      byte[] receivebuffer = new byte[1024];
+      
+      System.out.print("\nAvishak: ");
+      String clientData = sc.nextLine();
+      sendbuffer = clientData.getBytes();        
+      DatagramPacket sendPacket =new DatagramPacket(sendbuffer, sendbuffer.length, IP, 5006);
+      clientSocket.send(sendPacket);
+      if(clientData.equalsIgnoreCase("bye"))
+      {
+          System.out.println("Connection ended by Avishak");
+          break;
+      }
+      DatagramPacket receivePacket =
+      new DatagramPacket(receivebuffer, receivebuffer.length);
+      clientSocket.receive(receivePacket);
+      String serverData = new String(receivePacket.getData());
+      System.out.print("\nSania: " + serverData);
+      
+      //checking condition for equals with serverData which is my string
+      //c--;
+      }
+      clientSocket.close();
+    }
+    
+}
